1e16aef78b1daefcb596c5331191dd5eb6c2a810
XiaozhiSans <xiaozhisans-awa@outlook.com>
2024-06-11 16:41:24 +0800

    更新了一些有的没的(

--- static/aliveTime.js
+++ static/aliveTime.js
@@ -1,16 +1,16 @@
-function getAliveTime () {
+const getAliveTime = () => {
 	const lastUpdateDate = new Date('2024-03-22T15:25:15');
-	var currentTime = new Date();
-	var mins = (((currentTime - lastUpdateDate)/1000)/60);
-	var hours = parseInt(mins/60);
-	var min = parseInt(mins-(hours*60));
-	var s = parseInt((mins-parseInt(mins))*60);
-	var days = parseInt(hours/24);
-	var hour = parseInt(hours-(days*24));
-	var weeks = parseInt(days/7);
-	var day = parseInt(days-(weeks*7));
-	var aliveTime = (weeks+'周 '+day+'天 '+hour+'小时 '+min+'分钟 '+s+'秒');
+	let currentTime = new Date();
+	let mins = (((currentTime - lastUpdateDate)/1000)/60);
+	let hours = parseInt(mins/60);
+	let min = parseInt(mins-(hours*60));
+	let s = parseInt((mins-parseInt(mins))*60);
+	let days = parseInt(hours/24);
+	let hour = parseInt(hours-(days*24));
+	let weeks = parseInt(days/7);
+	let day = parseInt(days-(weeks*7));
+	let aliveTime = (`${weeks}周 ${day}天 ${hour}小时 ${min}分钟 ${s}秒`);
 
 	document.getElementById("aliveTime").innerHTML = aliveTime;
 }
-setInterval(function(){getAliveTime();},1000);
+const at = setInterval(getAliveTime, 1000);
--- static/main.js
+++ static/main.js
@@ -38,50 +38,15 @@ $('.kz-nav-btn').on('click', function() {
 
 console.log(
     "\n" +
-      " %c KZHomePage v1.2.0 by kaygb " +
-      " %c https://blog.170601.xyz/archives/25.html " +
+      " %c KZHomePage v1.2.0 (Modified) by XiaozhiSans" +
+      " %c https://xsawa.dev.tc/XiaozhiSans/ " +
       "\n" +
       "\n",
     "color: #fff; background: #fd79a8; padding:5px 0;",
     "background: #FFF; padding:5px 0;"
   );
-// 兼容旧版
-if(meting_music_api===""){
-    meting_api = "https://api.mizore.cn/meting/api.php";
-}
-var meting_api =
-  "https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id";
 
-$.ajax({
-//   url: "https://api.mizore.cn/meting/api.php?server=netease&type=playlist&id=20173709",
-  url: meting_music_api,
-  data:{
-    server: music_server,
-    type: music_type,
-    id: music_id
-  },
-  dataType: "json",
-  success: function (audio) {
-    const ap = new APlayer({
-        container: music_fixed === false ? document.getElementById('aplayer-inner') : document.getElementById('aplayer-fixed') ,
-        audio: audio,
-        fixed: music_fixed === false ? false : true,
-        autoplay: music_autoplay,
-        order: music_order,
-        listFolded :true,
-        volum: music_volume,
-        mini: music_fixed === true ? true:music_mini,
-        lrcType: 3,
-        preload:"auto",
-        loop: music_loop,
-        theme: player_color
-
-        
-    });
-  },
-});
-
-fetch('https://v1.hitokoto.cn')
+fetch('https://v1.hitokoto.cn?c=a&c=b&c=c&c=f')
     .then(response => response.json())
     .then(data => {
       const hitokoto = document.getElementById('hitokoto_text')
