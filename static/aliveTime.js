b0278510c1ca10061b0c5a9698f929da6c3620aa
XiaozhiSans <xiaozhisans-awa@outlook.com>
2024-10-27 16:49:29 +0800

    🎈 perf: improve

--- static/aliveTime.js
+++ static/aliveTime.js
@@ -1,17 +1,13 @@
-const at = (crtTime = new Date("2024-03-22T15:25:15"), target = document.querySelector("#aliveTime")) => {
+const at = (crtTime = new Date("2024-03-22T15:25:15"), target = document.querySelector("#alive-time")) => {
 	let curTime = new Date();
 	let mins = (((curTime - crtTime) / 1000) / 60);
 	let hours = parseInt(mins / 60);
 	let min = parseInt(mins - (hours * 60));
 	let s = parseInt((mins - parseInt(mins)) * 60);
 	let days = parseInt(hours / 24);
-	let hour = parseInt(hours - (days * 24));
-	let weeks = parseInt(days / 7);
-	let day = parseInt(days - (weeks * 7));
 
-	let m = parseInt(days / 30);
-	weeks -= parseInt(m * 4.286);
+	hours = hours - (days * 24);
 
-	target.innerText = `${m}月 ${weeks}周 ${day}天 ${hour}小时 ${min}分钟 ${s}秒`;
+	target.innerText = `${days}天 ${hours}小时 ${min}分钟 ${s}秒`;
 }
 const AT = setInterval(at, 1000);
--- static/main.js
+++ static/main.js
@@ -1,54 +1,33 @@
-var windowWidth = $(window).width();
-layer.config({
-  extend: 'kzhomepage/style.css', //加载扩展样式
-  skin: 'layer-ext-kzhomepage'
-});
-
-// Nav buttons
-$('.kz-nav-btn').on('click', function() {
-  let btn = $(this);
-  let type = btn.data('window') // pop current newtab
-  let content = btn.data('href')
-  switch (type) {
-    case 'pop':
-      let title = btn.data('title')
-      let shadeClose = btn.data('shade') === 'true' ? false : true
-      let anim = btn.data('anim') ? btn.data('anim')*1 : 4
-      let area_w = btn.data('area-w') ? btn.data('area-w') : '80%'
-      let area_h = btn.data('area-h') ? btn.data('area-h') : '90%'
-      layer.open({
-          type: 2,
-          title: title,
-          shadeClose: shadeClose,
-          anim:anim,
-          closeBtn: 2,
-          isOutAnim: false,
-          area: [area_w, area_h],
-          content: content
-      });
-      break;
-    case 'current':
-      window.location = content
-      break;
-    case 'newtab':
-      window.open('_blank').location = content
-      break;
-  }
-});
-
 console.log(
-    "\n" +
-      " %c KZHomePage v1.2.0-Modified by XiaozhiSans" +
-      " %c https://xsawa.dev.tc/XiaozhiSans/ \n" +
-      "\n",
+  `
+  %c KZHomePage v1.2.0 Modified by XiaozhiSans %c https://xsawa.us.kg/XiaozhiSans/
+`,
     "color: #fff; background: #fd79a8; padding: 5px 0;",
     "background: #fff; padding: 5px 0;"
 );
 
-fetch('https://v1.hitokoto.cn?c=a&c=b&c=c&c=f')
-    .then(response => response.json())
-    .then(data => {
-      const hitokoto = document.getElementById('hitokoto_text')
-      hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
-      hitokoto.title = hitokoto.innerText = data.hitokoto
-    }).catch(console.error)
+async function getHitokoto() {
+  const hitokoto = document.getElementById('hitokoto_text');
+  try {
+    const req = await fetch('https://v1.hitokoto.cn?c=a&c=b&c=c&c=f');
+    if(!req.ok) throw new Error(`Connection error: ${req.status}`);
+    const data = await req.json();
+    hitokoto.innerText = data.hitokoto;
+    hitokoto.title = data.from;
+    hitokoto.href = `https://hitokoto.cn/?uuid=${data.uuid}`;
+    hitokoto.target = '_blank';
+  } catch(e) {
+    console.error(`Failed to fetch hitokoto: ${e}`);
+  }
+}
+
+(()=>{
+  const btn = document.querySelector('#refresh_hitokoto');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  getHitokoto();
+  btn.classList.toggle('clicked');
+  setTimeout(() => {
+    btn.classList.toggle('clicked');
+  }, 400);
+});})()
